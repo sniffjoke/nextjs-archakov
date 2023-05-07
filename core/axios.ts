c9b5088c25ad9b3c14ff51c359ba617af4e7b817
sniffjoke <sniffjoke@gmail.com>
2023-05-07 23:07:29 +0200

    create dto add interceptors for axios

---
+++ core/axios.ts
@@ -0,0 +1,19 @@
+import axios, {AxiosRequestConfig} from "axios";
+import {parseCookies} from "nookies";
+
+
+axios.defaults.baseURL = 'http://localhost:7777'
+
+axios.interceptors.request.use((config)  => {
+
+    if (typeof window !== 'undefined') {
+        const {_token} = parseCookies()
+
+        config.headers.Authorization = "Bearer " + _token
+    }
+
+    return config
+
+})
+
+export default axios
