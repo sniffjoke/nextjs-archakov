9b5406972f245272f2a0cde1e63c80774c1e50b2
sniffjoke <sniffjoke@gmail.com>
2023-05-07 19:17:33 +0200

    first commit start project

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from './Auth.module.css'
+
+export const LoginForm: React.FC = () => {
+    return (
+        <div className={styles.root}>Форма авторизации</div>
+    )
+}
